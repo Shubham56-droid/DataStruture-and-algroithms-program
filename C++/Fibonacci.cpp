fe737980d1eec793d267717c1ad24d9933c6ac6c
Sheetal Lalwani <68683093+Sheetal0601@users.noreply.github.com>
2021-10-05 17:29:58 +0530

    Create Fibonacci

---
+++ C++/Fibonacci.cpp
@@ -0,0 +1,34 @@
+//Recursive
+
+ int fib(int n) {
+        if(n == 0)  return 0;
+        if(n == 1)  return 1;
+        return fib(n-1) + fib(n-2);
+    }
+
+
+//Iterative
+ int fib(int N) {
+        if(N < 2) 
+            return N;
+    	int a = 0, b = 1, c = 0;
+        for(int i = 1; i < N; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+
+
+//Dynamic Programming 
+ int fib(int n) {
+      vector<int> f(n+2,0);
+        f[0]=0; f[1]=1;
+        for(int i=2; i<=n; i++)
+        {
+            f[i]=f[i-1]+f[i-2];
+        }
+        return f[n];
+    }
