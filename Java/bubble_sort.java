cebc29a1d971013824314c6846d1bab5ebf065cb
Ujwalgulhane <72155234+Ujwalgulhane@users.noreply.github.com>
2021-10-07 09:39:51 +0530

    code is optimal and array added in code
    array is missing I will added array in this code

--- Java/bubble_sort.java
+++ Java/bubble_sort.java
@@ -14,4 +14,23 @@ class BubbleSort
                     arr[j+1] = temp;
                 }
     }
+ 
+    /* Prints the array in compilers*/
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+ 
+    // Driver method to test above
+    public static void main(String args[])
+    {
+        BubbleSort ob = new BubbleSort();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
 }
