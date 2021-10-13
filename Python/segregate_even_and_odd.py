df542a7209ad27fcd4b67e724a8597bad88d4ad3
Ujwal Gulhane <72155234+Ujwalgulhane@users.noreply.github.com>
2021-10-13 13:00:19 +0530

    program is optimal
    some variable changes and program is optimal

--- Python/segregate_even_and_odd.py
+++ Python/segregate_even_and_odd.py
@@ -1,19 +1,31 @@
-def isEven(x):
-    return x % 2
+# Python program to segregate even and odd elements of array
+
+def segregateEvenOdd(arr):
+
+	# Initialize left and right indexes
+	left,right = 0,len(arr)-1
+
+	while left < right:
+
+		# Increment left index while we see 0 at left
+		while (arr[left]%2==0 and left < right):
+			left += 1
+
+		# Decrement right index while we see 1 at right
+		while (arr[right]%2 == 1 and left < right):
+			right -= 1
+
+		if (left < right):
+			# Swap arr[left] and arr[right]*/
+			arr[left],arr[right] = arr[right],arr[left]
+			left += 1
+			right = right-1
 
 
-n = int(input())
-arr = [int(x) for x in input().split()]
-i = 0
-j = n - 1
-while(i < j):
-    while(isEven(arr[i]) and i < j):
-        i += 1
-    while(not isEven(arr[j]) and i < j):
-        j -= 1
-    if(i < j):
-        arr[i], arr[j] = arr[j], arr[i]
-        i += 1
-        j -= 1
-
-print(*arr)
+# Driver function to test above function
+arr = [12, 34, 45, 9, 8, 90, 3]
+segregateEvenOdd(arr)
+print ("Array after segregation "),
+for i in range(0,len(arr)):
+	print arr[i],
+# This code is contributed by Devesh Agrawal
